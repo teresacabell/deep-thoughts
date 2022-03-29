54e35f96cb818f67f516e2523fd80eeb2ee61008
teresacabell <teresacabll@gmail.com>
2022-03-29 12:40:53 -0500

    finished 21.2

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
@@ -18,6 +19,12 @@ const startServer = async () => {
 };
 
 startServer()
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
